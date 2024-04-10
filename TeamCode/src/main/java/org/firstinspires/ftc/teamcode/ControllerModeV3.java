06f43a94579a5a1c3dec8dc6908bb50b34aaff54
Dominik Bach <bachdominik06@icloud.com>
2024-04-10 09:02:29 -0500

    Code Started

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ControllerModeV3.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Drive")
+public class ControllerModeV3<telemetry> extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        while (opModeIsActive()) {
+        }
+    }
+}
