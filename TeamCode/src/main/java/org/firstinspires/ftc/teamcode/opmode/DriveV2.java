8bc3000fe1df364a915a26e2b124b6317fd1f6d5
Dominik Bach <bachdominik06@icloud.com>
2024-04-22 17:15:27 -0500

    Rename

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/DriveV2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/DriveV2.java
@@ -5,6 +5,8 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.drive.DriveTrain;
 import org.firstinspires.ftc.teamcode.robot.*;
 
 @TeleOp(name = "Drive V2")
@@ -13,10 +15,7 @@ public class DriveV2 extends LinearOpMode {
     boolean planeLaunched = false;
 
     //these are all our motors
-    private DcMotor FrontL;
-    private DcMotor BackL;
-    private DcMotor FrontR;
-    private DcMotor BackR;
+
     //servos
     private Servo claw;
     private Servo launcher;
@@ -31,12 +30,9 @@ public class DriveV2 extends LinearOpMode {
 
     @Override
     public void runOpMode() {
+        //Create the robot classes
+        DriveTrain mecanumWheels = new DriveTrain(this);
 
-        //these are for the movement values
-        float leftStickY;
-        float leftStickX;
-        double driveSpeed;
-        float pivot;
 
         //open and closed position of the gate, launcher, etc
         final double clawOpen = .32;
@@ -45,38 +41,20 @@ public class DriveV2 extends LinearOpMode {
         final double docked = 0;
 
         //this is all hardwaremaps
-        FrontL = hardwareMap.get(DcMotor.class, "FrontL");
-        BackL = hardwareMap.get(DcMotor.class, "BackL");
-        FrontR = hardwareMap.get(DcMotor.class, "FrontR");
-        BackR = hardwareMap.get(DcMotor.class, "BackR");
+
         claw = hardwareMap.get(Servo.class, "GateServo");
         launcher = hardwareMap.get(Servo.class, "LauncherServo");
         arm = new ArmPipeline(this);
 
-        FrontR.setDirection(DcMotorSimple.Direction.REVERSE);
-        BackR.setDirection(DcMotorSimple.Direction.REVERSE);
+
         waitForStart();
 
 
         while (opModeIsActive()) {
             // Put run blocks here that get looped
 
-            //this is all the drive code
-            if (gamepad1.right_trigger == 1) {
-                driveSpeed = 1.0;
-            }
-            else if(gamepad1.left_trigger == 1){
-                driveSpeed = 0.25;
-            } else {
-                driveSpeed = 0.5;
-            }
-            leftStickY = -gamepad1.left_stick_y;
-            leftStickX = gamepad1.left_stick_x;
-            pivot = gamepad1.right_stick_x;
-            FrontR.setPower((-pivot + (leftStickY - leftStickX)) * driveSpeed);
-            BackR.setPower((-pivot + leftStickY + leftStickX) * driveSpeed);
-            FrontL.setPower((pivot + leftStickY + leftStickX) * driveSpeed);
-            BackL.setPower((pivot + (leftStickY - leftStickX)) * driveSpeed);
+            mecanumWheels.drive(this);
+
 
             //everything that goes after this is for the gamepad 2
 
