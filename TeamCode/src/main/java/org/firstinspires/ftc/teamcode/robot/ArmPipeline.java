560ccfe834ed8d2dafebfed9e515bcaa83e17bc8
DomDBomb4Life <bachdominik06@icloud.com>
2024-04-22 16:54:16 -0500

    added Lukes controllermodeV3 and broken lift pipeline

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/ArmPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/ArmPipeline.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.robot;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class ArmPipeline {
@@ -15,8 +16,13 @@ public class ArmPipeline {
     private OpMode opmode;
 
     String startingPoint = "home";
+    private final double wristBackdropPos = 0.2699999;
+    private final double wristHome = 0.53;
 
     String state = "raise";
+
+    int[] liftPositions= new int[4];
+
 
     //costructor
     public ArmPipeline(OpMode opmode){
@@ -25,9 +31,19 @@ public class ArmPipeline {
         //init the motors
         liftL = opmode.hardwareMap.get(DcMotor.class, "LiftL");
         liftR = opmode.hardwareMap.get(DcMotor.class, "LiftR");
+        liftR.setDirection(DcMotorSimple.Direction.REVERSE);
         arm = opmode.hardwareMap.get(DcMotor.class, "Arm");
+        liftR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        liftL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         //init the wrist
         wrist = opmode.hardwareMap.get(Servo.class, "Wrist");
+        //init the lift heights
+        liftPositions[0] = 0;
+        liftPositions[1] = 50;
+        liftPositions[2] = -500;
+        liftPositions[3] = 5500;
+
     }
 
     public void moveLift(){
@@ -37,29 +53,47 @@ public class ArmPipeline {
                 //test the starting point of the lift
                 switch(startingPoint){
                     case "home":
-
+                        arm.setTargetPosition(liftPositions[1]);
+                        startingPoint = "safety";
                         break;
                     case "safety":
+                        liftR.setTargetPosition(liftPositions[3]);
+                        liftL.setTargetPosition(liftPositions[3]);
+                        startingPoint = "top";
                         break;
                     case "top":
+                        arm.setTargetPosition(liftPositions[2]);
+                        wrist.setPosition(wristBackdropPos);
                         break;
                     case "back board":
+                        state = "lower";
                         break;
                 }
             }else if(state == "lower"){
                 //test the starting point of the lift
                 switch(startingPoint){
                     case "home":
-
+                        state = "raise";
                         break;
                     case "safety":
+                        arm.setTargetPosition(liftPositions[0]);
+                        startingPoint = "home";
                         break;
                     case "top":
+                        liftR.setTargetPosition(liftPositions[0]);
+                        liftL.setTargetPosition(liftPositions[0]);
+                        startingPoint = "safety";
                         break;
                     case "back board":
+                        arm.setTargetPosition(liftPositions[1]);
+                        wrist.setPosition(wristHome);
+                        startingPoint = "top";
                         break;
                 }
             }
+            liftR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            liftL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         }
     }
 
