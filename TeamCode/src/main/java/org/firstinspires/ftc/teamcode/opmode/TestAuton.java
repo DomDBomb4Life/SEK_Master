2dae3b3d5b4148585e72940c737c39be64c69ba4
Dominik Bach <bachdominik06@icloud.com>
2024-05-06 16:27:53 -0500

    Auton Test Stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/TestAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/TestAuton.java
@@ -4,41 +4,14 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.util.VisionPipeline;
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
-import org.opencv.imgproc.Imgproc;
-import org.openftc.easyopencv.OpenCvPipeline;
+import org.firstinspires.ftc.teamcode.util.pipelines.PixelLocator;
 
 @Autonomous
 public class TestAuton extends LinearOpMode {
-    testPipeline pipe = new testPipeline();
+    PixelLocator pipe = new PixelLocator();
     VisionPipeline eyes = new VisionPipeline(this, pipe);
 
     @Override
     public void runOpMode() {
-    }
-
-
-    class testPipeline extends OpenCvPipeline {
-        //put your variables here to use later
-
-        @Override// this is the method that tells the pipeline what to do with an image
-        public Mat processFrame(Mat input) {
-            //apply a filter to the image to make it so all of the white image pixels are represented as 1s in a binary image.
-            // Convert the image to grayscale
-            Mat gray = new Mat();
-            Imgproc.cvtColor(input, gray, Imgproc.COLOR_BGR2GRAY);
-
-            // Apply a threshold to create a binary image
-            Mat binary = new Mat();
-            Imgproc.threshold(gray, binary, 0, 255, Imgproc.THRESH_BINARY);
-
-            // Invert the binary image
-            Core.bitwise_not(binary, binary);
-
-            // Set the result as the processed frame
-            input = binary;
-            return input;
-        }
     }
 }
